3997374c502b2df5bde6eb6dfc76a7afb40dd492
SuperArbor <snrihc219@hotmail.com>
2023-05-20 19:41:20 +0000

    Update from https://github.com/SuperArbor/pacrules/commit/c5661bd876322fc22ddb06dda1d0195ad2d1dc47

--- pac.js
+++ pac.js
@@ -1,7 +1,7 @@
 /**
  * --PACRULES--
- * Generated: 2023-05-19 19:41:21
- * GFWList Last-Modified: 2023-03-26 13:35:48
+ * Generated: 2023-05-20 19:41:18
+ * GFWList Last-Modified: 2023-05-20 03:42:18
  * GFWList From: https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
  */
 
@@ -3423,6 +3423,7 @@ var rules = [
             ["dnscrypt.org","DOMAIN-SUFFIX"],
             ["dnssec.net","DOMAIN-SUFFIX"],
             ["dnvod.tv","DOMAIN-SUFFIX"],
+            ["docker.com","DOMAIN-SUFFIX"],
             ["doctorvoice.org","DOMAIN-KEYWORD"],
             ["documentingreality.com","DOMAIN-SUFFIX"],
             ["dolc.de","DOMAIN-SUFFIX"],
@@ -5680,6 +5681,7 @@ var rules = [
             ["ozvoice.org","DOMAIN-SUFFIX"],
             ["pachosting.com","DOMAIN-SUFFIX"],
             ["pacopacomama.com","DOMAIN-SUFFIX"],
+            ["page.link","DOMAIN-SUFFIX"],
             ["page2rss.com","DOMAIN-KEYWORD"],
             ["pages.dev","DOMAIN-SUFFIX"],
             ["pagodabox.com","DOMAIN-SUFFIX"],
@@ -5792,6 +5794,7 @@ var rules = [
             ["podbean.com","DOMAIN-SUFFIX"],
             ["podcast.co","DOMAIN-SUFFIX"],
             ["podictionary.com","DOMAIN-SUFFIX"],
+            ["poe.com","DOMAIN-SUFFIX"],
             ["pokerstars.com","DOMAIN-SUFFIX"],
             ["politicalchina.org","DOMAIN-KEYWORD"],
             ["politicalconsultation.org","DOMAIN-KEYWORD"],
