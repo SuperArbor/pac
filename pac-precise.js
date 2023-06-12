744333b4ffd1cdb0999d04c36838e0691df4a14e
SuperArbor <snrihc219@hotmail.com>
2023-06-12 19:43:27 +0000

    Update from https://github.com/SuperArbor/pacrules/commit/1b13b194c8af0ad7e52638a2556d7efd6c804c28

--- pac-precise.js
+++ pac-precise.js
@@ -1,6 +1,6 @@
 /**
  * --PACRULES--
- * Generated: 2023-06-11 19:43:46
+ * Generated: 2023-06-12 19:43:24
  * GFWList Last-Modified: 2023-05-20 03:42:18
  * GFWList From: https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
  */
