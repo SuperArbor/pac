8be4a6fa146d5391162a22119e70cabca34027a4
SuperArbor <snrihc219@hotmail.com>
2026-08-21 03:22:29 +0800

    Update from local commit

--- pac-precise.js
+++ pac-precise.js
@@ -1,6 +1,6 @@
 /**
  * --PACRULES--
- * Generated: 2026-08-21 01:46:38
+ * Generated: 2026-08-21 03:22:25
  * GFWList Last-Modified: 2026-08-19 10:15:34
  * GFWList From: https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
  */
