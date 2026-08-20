35b98bb08c61ea3682ffb455a0b4c769c833b22e
SuperArbor <snrihc219@hotmail.com>
2026-08-21 03:58:15 +0800

    Update from local commit

--- pac.js
+++ pac.js
@@ -1,6 +1,6 @@
 /**
  * --PACRULES--
- * Generated: 2026-08-21 03:35:29
+ * Generated: 2026-08-21 03:58:11
  * GFWList Last-Modified: 2026-08-19 10:15:34
  * GFWList From: https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
  */
